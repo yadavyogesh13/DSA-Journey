8c5a03da0a41c16e6d095d6ff0dcd5594e7c1161
yy0213148 <43162443+yy0213148@users.noreply.github.com>
2026-01-14 22:04:15 +0530

    square and right angle

---
+++ loops/star-patters/SquareStar.java
@@ -0,0 +1,9 @@
+public class SquareStar {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for(int i = 0; i < n; i++){
+            System.out.println("****");
+        }
+    }
+}
